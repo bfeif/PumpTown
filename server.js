6a3426f2c84ba378d77f374c21607c7474591223
Ben Feifke <benfeifke@gmail.com>
2019-01-25 14:53:04 -0500

    More heroku deploy fixes.

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/pump-town'));
+app.use(express.static('/dist/pump-town'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/pump-town/index.html'));
+res.sendFile(path.join('/dist/PumpTown/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
