0742c8329a3a1c06decf168381e351eaeffd5b22
Ben Feifke <benfeifke@gmail.com>
2019-01-31 22:17:25 -0500

    Cleaned file.

--- src/app/pumps.service.ts
+++ src/app/pumps.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
-
 import { Observable, of } from 'rxjs';
-
 import { Pump } from './pump';
-import { PUMPS } from './mock-pumps';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, map, tap } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
