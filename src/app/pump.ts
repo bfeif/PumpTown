b296bbf7784927ac2f2226a49e72dc397e77edd4
Ben Feifke <benfeifke@gmail.com>
2019-01-31 22:54:30 -0500

    Added 'checked' attribute to pump so I can manually check added pumps.

--- src/app/pump.ts
+++ src/app/pump.ts
@@ -3,11 +3,13 @@ export class Pump {
     lat: number;
     lon: number;
     notes: string;
+    checked: boolean;
 
     constructor(lat, lon, notes) {
-        // this.id = id;
+        // this.id = id; // this is automatically auto-incremented by json-server and is unnecessary to put here
         this.lat = lat;
         this.lon = lon;
         this.notes = notes;
+        this.checked = false; 
     }
 }
