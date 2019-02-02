ba31b944424d3e1f1ffe91fd12b49873a96300ce
Ben Feifke <benfeifke@gmail.com>
2019-02-02 18:23:57 -0500

    fixed mock pump for new pump schema.

--- src/app/mock-pumps.ts
+++ src/app/mock-pumps.ts
@@ -1,7 +1,7 @@
 import { Pump } from './pump';
 
 export const PUMPS: Pump[] = [
-    { lat: 42.381047, lon: -71.099001, notes: "Right outside Somervelo." },
-    { lat: 42.367979, lon: -71.102072, notes: "Right outside Whole Foods."},
-    { lat: 42.361754, lon: -71.097386, notes: "Right outside Cambridge Bicycle."}
+    { lat: 42.381047, lon: -71.099001, notes: "Right outside Somervelo.", checked: true},
+    { lat: 42.367979, lon: -71.102072, notes: "Right outside Whole Foods.", checked: true},
+    { lat: 42.361754, lon: -71.097386, notes: "Right outside Cambridge Bicycle.", checked: true}
 ];
