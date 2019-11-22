84b4e39992b9106e8e79fe02fafc2834551d15eb
Ben Feifke <benfeifke@gmail.com>
2019-11-22 16:31:57 -0500

    reverting last couple commits. t Please enter the commit message for your changes. Lines starting

--- src/app/pumps.service.ts
+++ src/app/pumps.service.ts
@@ -13,6 +13,7 @@ const httpOptions = {
 })
 export class PumpsService {
 
+  // private pumpsUrl = 'http://my-json-server.typicode.com/bfeif/PumpTownDB/pumps/'; // replace this with some json file
   // private pumpsUrl = 'http://localhost:3000/pumps/'; // local json server
   private pumpsUrl = 'https://pump-town-db.herokuapp.com/pumps'; // heroku json server
   constructor(private http: HttpClient) { }
